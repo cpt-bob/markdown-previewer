7cccf17c9e8f6e3663c5c8ebcee6fcf4f0e444c7
ltbob <cpt.bob@gmail.com>
2024-08-26 11:09:50 -0600

    establish outline for all items needed for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor-container">
+        <div className="toolbar"></div>
+        <textarea id="editor" type="text"></textarea>
+      </div>
+      <div className="converter"></div>
+      <div className="preview-container">
+        <div className="toolbar"></div>
+        <div className="preview" id="preview">
+          <h1></h1>
+          <h2></h2>
+          <a></a>
+          <p id="inline-code"></p>
+          <p id="code-block"></p>
+          <li></li>
+          <blockquote></blockquote>
+          <img />
+          <p id="bolded-text"></p>
+        </div>
+      </div>
     </div>
   );
 }
